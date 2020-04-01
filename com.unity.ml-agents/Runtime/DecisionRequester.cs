1c3369e770fc14ca41331873b3720a232a17274c
Christopher Goy <christopherg@unity3d.com>
2020-04-01 15:06:10 -0700

    Rename Academy.AgentSetStatus to Academy.AgentPreStep and make it public to allow users to hook into the Environment step.  This will allow them to write custom logic to RequestDecisions/Actions for their Agents and guarantee that those are reflected in the appropriate step.

--- com.unity.ml-agents/Runtime/DecisionRequester.cs
+++ com.unity.ml-agents/Runtime/DecisionRequester.cs
@@ -13,32 +13,11 @@ namespace MLAgents
     {
         /// <summary>
         /// The frequency with which the agent requests a decision. A DecisionPeriod of 5 means
-        /// that the Agent will request a decision every 5 Academy steps.
-        /// </summary>
+        /// that the Agent will request a decision every 5 Academy steps. /// </summary>
         [Range(1, 20)]
         [Tooltip("The frequency with which the agent requests a decision. A DecisionPeriod " +
                  "of 5 means that the Agent will request a decision every 5 Academy steps.")]
         public int DecisionPeriod = 5;
-
-        /// <summary>
-        /// A delegate that can be used to customize the logic of when your Agent gets its
-        /// <see cref="Agent.RequestDecision"/> and <see cref="Agent.RequestAction"/> called.
-        /// If this value is not get, the default logic will use <see cref="DecisionPeriod"/> and
-        /// <see cref="TakeActionsBetweenDecisions"/> in order to decide when to call those two
-        /// methods on your Agent.
-        /// </summary>
-        /// <param name="academyStepCount"> The <see cref="Academy.StepCount"/>.</param>
-        /// <param name="decisionPeriod">The <see cref="DecisionRequester.DecisionPeriod"/> of the calling
-        /// <see cref="DecisionRequester"/>.</param>
-        /// <param name="takeActionsBetweenDecisions">The <see cref="DecisionRequester.TakeActionsBetweenDecisions"/>
-        /// of the calling <see cref="DecisionRequester"/></param>
-        public delegate void MakeRequestsDelegate(int academyStepCount, int decisionPeriod, bool takeActionsBetweenDecisions);
-
-        /// <summary>
-        /// The <see cref="MakeRequestsDelegate"/>" that can optionally be used to make calls to
-        /// <see cref="Agent.RequestDecision"/> and/or <see cref="Agent.m_RequestAction"/>.
-        /// </summary>
-        public MakeRequestsDelegate makeRequestsDelegate;
 
         /// <summary>
         /// Indicates whether or not the agent will take an action during the Academy steps where
@@ -56,14 +35,14 @@ namespace MLAgents
         {
             m_Agent = gameObject.GetComponent<Agent>();
             Debug.Assert(m_Agent != null, "Agent component was not found on this gameObject and is required.");
-            Academy.Instance.AgentSetStatus += MakeRequests;
+            Academy.Instance.AgentPreStep += MakeRequests;
         }
 
         void OnDestroy()
         {
             if (Academy.IsInitialized)
             {
-                Academy.Instance.AgentSetStatus -= MakeRequests;
+                Academy.Instance.AgentPreStep -= MakeRequests;
             }
         }
 
@@ -74,12 +53,6 @@ namespace MLAgents
         /// <param name="academyStepCount">The current step count of the academy.</param>
         void MakeRequests(int academyStepCount)
         {
-            if (makeRequestsDelegate != null)
-            {
-                makeRequestsDelegate(academyStepCount, DecisionPeriod, TakeActionsBetweenDecisions);
-                return;
-            }
-
             if (academyStepCount % DecisionPeriod == 0)
             {
                 m_Agent?.RequestDecision();
