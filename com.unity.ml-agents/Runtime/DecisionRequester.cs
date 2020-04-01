3caa0038805fcbf503f3138e14b8e9bf5e9b2640
Christopher Goy <christopherg@unity3d.com>
2020-04-01 14:24:17 -0700

    [API] Make the DecisionRequester public and add a delegate to its API to allow users to customize it's behavior.

--- com.unity.ml-agents/Runtime/DecisionRequester.cs
+++ com.unity.ml-agents/Runtime/DecisionRequester.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -8,7 +9,7 @@ namespace MLAgents
     /// at regular intervals.
     /// </summary>
     [AddComponentMenu("ML Agents/Decision Requester", (int)MenuGroup.Default)]
-    internal class DecisionRequester : MonoBehaviour
+    public class DecisionRequester : MonoBehaviour
     {
         /// <summary>
         /// The frequency with which the agent requests a decision. A DecisionPeriod of 5 means
@@ -20,6 +21,26 @@ namespace MLAgents
         public int DecisionPeriod = 5;
 
         /// <summary>
+        /// A delegate that can be used to customize the logic of when your Agent gets its
+        /// <see cref="Agent.RequestDecision"/> and <see cref="Agent.RequestAction"/> called.
+        /// If this value is not get, the default logic will use <see cref="DecisionPeriod"/> and
+        /// <see cref="TakeActionsBetweenDecisions"/> in order to decide when to call those two
+        /// methods on your Agent.
+        /// </summary>
+        /// <param name="academyStepCount"> The <see cref="Academy.StepCount"/>.</param>
+        /// <param name="decisionPeriod">The <see cref="DecisionRequester.DecisionPeriod"/> of the calling
+        /// <see cref="DecisionRequester"/>.</param>
+        /// <param name="takeActionsBetweenDecisions">The <see cref="DecisionRequester.TakeActionsBetweenDecisions"/>
+        /// of the calling <see cref="DecisionRequester"/></param>
+        public delegate void MakeRequestsDelegate(int academyStepCount, int decisionPeriod, bool takeActionsBetweenDecisions);
+
+        /// <summary>
+        /// The <see cref="MakeRequestsDelegate"/>" that can optionally be used to make calls to
+        /// <see cref="Agent.RequestDecision"/> and/or <see cref="Agent.m_RequestAction"/>.
+        /// </summary>
+        public MakeRequestsDelegate makeRequestsDelegate;
+
+        /// <summary>
         /// Indicates whether or not the agent will take an action during the Academy steps where
         /// it does not request a decision. Has no effect when DecisionPeriod is set to 1.
         /// </summary>
@@ -29,23 +50,12 @@ namespace MLAgents
         [FormerlySerializedAs("RepeatAction")]
         public bool TakeActionsBetweenDecisions = true;
 
-        /// <summary>
-        /// Whether or not the Agent decisions should start at an offset (different for each agent).
-        /// This does not affect <see cref="DecisionPeriod"/>. Turning this on will distribute
-        /// the decision-making computations for all the agents across multiple Academy steps.
-        /// This can be valuable in scenarios where you have many agents in the scene, particularly
-        /// during the inference phase.
-        /// </summary>
-        [Tooltip("Whether or not Agent decisions should start at an offset.")]
-        public bool offsetStep;
-
         Agent m_Agent;
-        int m_Offset;
 
         internal void Awake()
         {
-            m_Offset = offsetStep ? gameObject.GetInstanceID() : 0;
             m_Agent = gameObject.GetComponent<Agent>();
+            Debug.Assert(m_Agent != null, "Agent component was not found on this gameObject and is required.");
             Academy.Instance.AgentSetStatus += MakeRequests;
         }
 
@@ -57,9 +67,20 @@ namespace MLAgents
             }
         }
 
-        void MakeRequests(int count)
+        /// <summary>
+        /// Method that hooks into the Academy in order inform the Agent on whether or not it should request a
+        /// decision, and whether or not it should take actions between decisions.
+        /// </summary>
+        /// <param name="academyStepCount">The current step count of the academy.</param>
+        void MakeRequests(int academyStepCount)
         {
-            if ((count + m_Offset) % DecisionPeriod == 0)
+            if (makeRequestsDelegate != null)
+            {
+                makeRequestsDelegate(academyStepCount, DecisionPeriod, TakeActionsBetweenDecisions);
+                return;
+            }
+
+            if (academyStepCount % DecisionPeriod == 0)
             {
                 m_Agent?.RequestDecision();
             }
